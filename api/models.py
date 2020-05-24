c9701cc6ce0abea5fec0c6d79c2a6e148e2b6cbd
dave <dave1.t.wells@gmail.com>
2020-05-24 11:27:02 -0600

    Added materialize, commented out old code to be refactored.

--- api/models.py
+++ api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Nav(models.Model):
     name = models.CharField(max_length=100, primary_key=True)
-    fa_icon = models.CharField(max_length=100)
+    icon = models.CharField(max_length=100)
     path = models.URLField(max_length=200)
 
     def __str__(self):
